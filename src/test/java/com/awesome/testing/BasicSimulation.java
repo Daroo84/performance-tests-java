b7941297a7de148f5c02ad85c0dc759685e9b36e
Dariusz Staroń <dstaron1@intercars.eu>
2024-05-28 13:56:19 +0200

    Added Stress test

--- src/test/java/com/awesome/testing/BasicSimulation.java
+++ src/test/java/com/awesome/testing/BasicSimulation.java
@@ -17,13 +17,22 @@ public class BasicSimulation extends Simulation {
 
     private static final int DESIRED_RPM = 60;
     private static final int SECONDS_IN_MINUTE = 60;
-    private static final int RPS = DESIRED_RPM / SECONDS_IN_MINUTE;
+    private static final double RPS = (double) DESIRED_RPM / SECONDS_IN_MINUTE;
 
     {
         setUp(TRAINING_SCENARIO.injectOpen(
-                        rampUsersPerSec(0).to(RPS).during(Duration.ofMinutes(2)),
-                        constantUsersPerSec(RPS).during(Duration.ofMinutes(4)).randomized(),
-                        rampUsersPerSec(RPS).to(0).during(Duration.ofMinutes(2))
+                        rampUsersPerSec(0).to(0.5).during(Duration.ofMinutes(30)),
+                        constantUsersPerSec(0.5).during(Duration.ofMinutes(30)),
+
+                        rampUsersPerSec(0.5).to(1).during(Duration.ofMinutes(30)),
+                        constantUsersPerSec(1).during(Duration.ofMinutes(30)),
+
+                        rampUsersPerSec(1).to(1.5).during(Duration.ofMinutes(30)),
+                        constantUsersPerSec(1).during(Duration.ofMinutes(30)),
+
+                        rampUsersPerSec(1.5).to(2).during(Duration.ofMinutes(30)),
+                        constantUsersPerSec(2).during(Duration.ofMinutes(30))
+
                 )
                 .protocols(HTTP_CONFIG))
                 .assertions(GLOBAL_ASSERTIONS);
